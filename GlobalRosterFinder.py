75ae3338aa92c32a87c87916e4ee4d4c81f87410
Duocast <65754673+Duocast@users.noreply.github.com>
2023-04-18 15:12:15 -0600

    Update GlobalRosterFinder.py

--- GlobalRosterFinder.py
+++ GlobalRosterFinder.py
@@ -5,13 +5,13 @@ from pathlib import Path
 import csv
 
 REQUIRED_COLUMNS = {
-    'Employee Number',
-    'Employee Name',
-    'Current Hire Date',
-    'Work Country',
-    'Business Title',
-    'Email Address',
-    'Business Group'
+    'employee number',
+    'employee name',
+    'current hire date',
+    'work country',
+    'business title',
+    'email address',
+    'business group'
 }
 
 def find_matching_files(start_directory, valid_extensions=None):
@@ -19,7 +19,8 @@ def find_matching_files(start_directory, valid_extensions=None):
         valid_extensions = ['.csv']
 
     def contains_required_columns(columns):
-        return REQUIRED_COLUMNS.issubset(set(columns))
+        columns_set = {col.strip().lower().replace('\ufeff', '') for col in columns}
+        return REQUIRED_COLUMNS.issubset(columns_set)
 
     for root, _, files in os.walk(start_directory):
         for file in files:
@@ -28,10 +29,16 @@ def find_matching_files(start_directory, valid_extensions=None):
                 file_path = os.path.join(root, file)
                 try:
                     with open(file_path, 'r', encoding='utf-8', errors='ignore') as csvfile:
+                        print(f"Processing file: {file_path}")
                         reader = csv.reader(csvfile)
                         header = next(reader)
                         if contains_required_columns(header):
+                            print(f"Found matching file: {file_path}")
                             yield file_path
+                        else:
+                            missing_columns = REQUIRED_COLUMNS - {col.strip().lower() for col in header}
+                            print(f"File {file_path} does not contain the required columns: {missing_columns}")
+                            print(f"Header in file: {header}")
                 except Exception as e:
                     print(f"Error reading file {file_path}: {e}")
 
@@ -67,8 +74,12 @@ def main():
                 target_directory = os.path.join(user_path, user_folder)
                 if os.path.exists(target_directory):
                     for matched_file in find_matching_files(target_directory, valid_extensions):
-                        dest_folder = get_destination_folder(shared_folder, hostname, user)
-                        copy_file_and_create_info_file(matched_file, dest_folder, hostname, user)
+                        print(f"Processing matched_file: {matched_file}")  # Added
+                        try:
+                            dest_folder = get_destination_folder(shared_folder, hostname, user)
+                            copy_file_and_create_info_file(matched_file, dest_folder, hostname, user)
+                        except Exception as e:
+                            print(f"Error copying file {matched_file} to {dest_folder}: {e}")
 
 if __name__ == "__main__":
     main()
