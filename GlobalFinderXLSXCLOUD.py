a751513050f1652f54120906388fe030a62d579b
Duocast <65754673+Duocast@users.noreply.github.com>
2023-04-19 15:16:04 -0600

    Update GlobalFinderXLSXCLOUD.py
    Added the ability to search in archives.

--- GlobalFinderXLSXCLOUD.py
+++ GlobalFinderXLSXCLOUD.py
@@ -17,27 +17,56 @@ REQUIRED_COLUMNS = {
 }
 
 def find_matching_files(target_directory, valid_extensions):
+    matching_files = []
     for root, _, files in os.walk(target_directory):
         for file in files:
-            if file.endswith(valid_extensions):
-                file_path = os.path.join(root, file)
-                try:
-                    if file_path.endswith('.xls'):
-                        engine = 'xlrd'
-                        df = pd.read_excel(file_path, engine=engine, nrows=0)
-                    elif file_path.endswith('.xlsx'):
-                        engine = 'openpyxl'
-                        df = pd.read_excel(file_path, engine=engine, nrows=0)
-                    elif file_path.endswith('.csv'):
-                        df = pd.read_csv(file_path, nrows=0)
-                    else:
-                        continue
+            file_path = os.path.join(root, file)
 
-                    if all(column in df.columns for column in REQUIRED_COLUMNS):
-                        yield file_path
+            if file.endswith(valid_extensions):
+                try:
+                    matching_file = process_matching_file(file_path)
+                    if matching_file:
+                        matching_files.append(matching_file)
                 except Exception as e:
                     print(f"Error reading file {file_path}: {e}. Skipping this file.")
                     continue
+
+            elif file.endswith('.zip'):
+                try:
+                    with zipfile.ZipFile(file_path, 'r') as archive:
+                        for archive_file in archive.namelist():
+                            if archive_file.endswith(valid_extensions):
+                                extracted_file_path = archive.extract(archive_file, os.path.join(target_directory, 'temp'))
+                                try:
+                                    matching_file = process_matching_file(extracted_file_path)
+                                    if matching_file:
+                                        matching_files.append(matching_file)
+                                except Exception as e:
+                                    print(f"Error reading file {extracted_file_path}: {e}. Skipping this file.")
+                                    continue
+                                finally:
+                                    os.remove(extracted_file_path)
+
+                except Exception as e:
+                    print(f"Error processing archive {file_path}: {e}. Skipping this file.")
+                    continue
+    return matching_files
+
+def process_matching_file(file_path):
+    if file_path.endswith('.xls'):
+        engine = 'xlrd'
+        df = pd.read_excel(file_path, engine=engine, nrows=0)
+    elif file_path.endswith('.xlsx'):
+        engine = 'openpyxl'
+        df = pd.read_excel(file_path, engine=engine, nrows=0)
+    elif file_path.endswith('.csv'):
+        df = pd.read_csv(file_path, nrows=0)
+    else:
+        return None
+
+    if all(column in df.columns for column in REQUIRED_COLUMNS):
+        return file_path
+    return None
 
 def copy_file_and_create_info_file(src, dest_folder, hostname, username):
     Path(dest_folder).mkdir(parents=True, exist_ok=True)
