76584f138b182657a7feb74729b435b07db5e8a3
Duocast <65754673+Duocast@users.noreply.github.com>
2023-04-20 10:20:56 -0600

    Update GlobalFinderXLSXCLOUD.py

--- GlobalFinderXLSXCLOUD.py
+++ GlobalFinderXLSXCLOUD.py
@@ -5,6 +5,7 @@ from pathlib import Path
 import pandas as pd
 from openpyxl import load_workbook
 import zipfile
+from datetime import datetime
 
 REQUIRED_COLUMNS = {
     'Employee Number',
@@ -34,22 +35,28 @@ def find_matching_files(target_directory, valid_extensions):
             elif file.endswith('.zip'):
                 try:
                     with zipfile.ZipFile(file_path, 'r') as archive:
+                        found_matching_file = False
                         for archive_file in archive.namelist():
-                            if archive_file.endswith(valid_extensions):
-                                extracted_file_path = archive.extract(archive_file, os.path.join(target_directory, 'temp'))
-                                try:
-                                    matching_file = process_matching_file(extracted_file_path)
-                                    if matching_file:
-                                        matching_files.append(matching_file)
-                                except Exception as e:
-                                    print(f"Error reading file {extracted_file_path}: {e}. Skipping this file.")
-                                    continue
-                                finally:
-                                    os.remove(extracted_file_path)
+                            with archive.open(archive_file) as af:
+                                temp_path = os.path.join(target_directory, 'temp', archive_file)
+                                parent = os.path.dirname(temp_path)
+                                os.makedirs(parent, exist_ok=True)
+                                with open(temp_path, "wb") as tf:
+                                    shutil.copyfileobj(af, tf)
+                                matching_file = process_matching_file(temp_path)
+                                os.remove(temp_path)
+
+                            if matching_file:
+                                found_matching_file = True
+                                break
+
+                        if found_matching_file:
+                            matching_files.append(file_path)
 
                 except Exception as e:
                     print(f"Error processing archive {file_path}: {e}. Skipping this file.")
                     continue
+                
     return matching_files
 
 def process_matching_file(file_path):
@@ -68,29 +75,32 @@ def process_matching_file(file_path):
         return file_path
     return None
 
-def copy_file_and_create_info_file(src, dest_folder, hostname, username):
+def copy_file_and_create_info_file(src, dest_folder, hostname, username, delete_after_copy=False):
     Path(dest_folder).mkdir(parents=True, exist_ok=True)
 
     dest_file = os.path.join(dest_folder, os.path.basename(src))
     shutil.copy(src, dest_file)
 
+    if delete_after_copy:
+        os.remove(src)
+
     info_filename = os.path.basename(src) + "_info.txt"
     info_filepath = os.path.join(dest_folder, info_filename)
 
     file_stat = os.stat(src)
-    file_size = file_stat.st_size
-    file_creation_date = file_stat.st_ctime
-    file_last_modified_date = file_stat.st_mtime
-    file_last_accessed_date = file_stat.st_atime
-
+    file_size_kb = file_stat.st_size / 1024
+    file_creation_date = datetime.fromtimestamp(file_stat.st_ctime).strftime('%Y-%m-%d %H:%M:%S')
+    file_last_modified_date = datetime.fromtimestamp(file_stat.st_mtime).strftime('%Y-%m-%d %H:%M:%S')
+    file_last_accessed_date = datetime.fromtimestamp(file_stat.st_atime).strftime('%Y-%m-%d %H:%M:%S')
+    
     with open(info_filepath, 'w') as f:
         f.write(f"File location: {src}\n")
         f.write(f"Hostname: {hostname}\n")
         f.write(f"Username: {username}\n")
-        f.write(f"File size: {file_size} bytes\n")
-        f.write(f"File creation date: {os.path.getctime(src)}\n")
-        f.write(f"File last modified date: {os.path.getmtime(src)}\n")
-        f.write(f"File last accessed date: {os.path.getatime(src)}\n")
+        f.write(f"File size: {file_size_kb:.2f} KB\n")
+        f.write(f"File creation date: {file_creation_date}\n")
+        f.write(f"File last modified date: {file_last_modified_date}\n")
+        f.write(f"File last accessed date: {file_last_accessed_date}\n")
 
 def get_destination_folder(base_folder, hostname, username):
     return os.path.join(base_folder, f"{hostname}.{username}")
